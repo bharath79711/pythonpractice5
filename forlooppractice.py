b01d357366091f6d6d588cf1236955a217b22e62
Bharath79711 <bharathkumartalari70@gmail.com>
2025-07-17 06:06:59 +0530

    for loop condition practice

---
+++ forlooppractice.py
@@ -0,0 +1,63 @@
+# Ex:-1
+'''
+fruits = ['apple','banana','cherry']
+for fruit in fruits:
+    print(f"i love{fruit}!")
+'''
+# Ex:-2
+'''
+for i in range(5):
+    print(f"Before Number:{i}")
+    if i==3:
+        break
+    print(f"After Number:{i}")
+'''
+# Ex:-3
+'''
+for char in "python":
+    print(char.title())
+'''
+# Ex:-4
+'''
+for char in "hell oworld":
+    if char==" ":
+        print("Space found!Ending loop.")
+        break
+    print(char.title())
+'''
+# Ex:-5
+'''
+
+for num in range(10):
+    if num % 2==0:
+    print(f"{num}is even")
+'''
+# Ex-6
+
+for grade in range(10,100,5):
+    if grade>=75:
+        print(f"{grade}:Excellent")
+    elif grade>=50:
+        print(f"{grade}:pass")
+    else:
+        print(f"{grade}:Fail")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
